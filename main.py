2dcc56de1107aed26f3b8f8f6d0b555c2f953aa0
im-yoursaviour <akowuahnimoh@gmail.com>
2020-09-20 13:49:56 +0000

    added images

--- main.py
+++ main.py
@@ -1,25 +1,33 @@
-import sys,pygame
+import sys, pygame
+from settings import Settings
+
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior"""
+
     def __init__(self):
         """ Initialize game and create game resources"""
         pygame.init()
 
-        self.screen = pygame.display.set_mode(size=(1200,800))
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode(size=(self.settings.screen_width, self.settings.screenHeight))
+        pygame.display.set_caption("Alien invasion")
+
 
     def run_game(self):
         """ Start the main loop for the game"""
         while True:
-            #watch for keyboard and mouse events
+            # watch for keyboard and mouse events
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-
-            #make the most recently drawn screen visible .
+            self.screen.fill(self.settings.bg_color)
+            # make the most recently drawn screen visible .
             pygame.display.flip()
+
+
 
 if __name__ == '__main__':
     ai = AlienInvasion()
     ai.run_game()
-print("hello wold")
+
---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings:
+    """ A class to store all settings for Alien Invation"""
+
+    def __init__(self):
+        """Initialize game settings"""
+        #screen settings
+        self.screen_width = 1200
+        self.screenHeight = 800
+        self.bg_color = (230, 230, 230)
